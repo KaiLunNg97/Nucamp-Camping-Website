2f0c18eace7e09c24c99262642f7e31c8d25bd96
Kai <tringuyenkhac97@yahoo.com>
2022-07-29 21:43:56 -0700

    nucamp camping site
    first ed

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(function() {
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($('#carouselButton').children('i').hasClass('fa-pause')) { 
+            //Check if the button has a child node i with the class fa-pause
+            $('.carousel').carousel('pause');
+            $('#carouselButton').children('i').removeClass('fa-pause'); 
+            $('#carouselButton').children('i').addClass('fa-play');
+            //remove then add class to switch out pause to play icon on font awesome
+        } else {
+            $('.carousel').carousel('cycle');
+            $('#carouselButton').children('i').removeClass('fa-play');
+            $('#carouselButton').children('i').addClass('fa-pause');
+        } 
+    });
+});
